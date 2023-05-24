e7022aea06a7d45c4358ec88ee2da0961d1c3041
yuna096740 <yyuusseeii1111@gmail.com>
2023-05-24 23:50:47 +0000

    Initial Commit

---
+++ bookers2_789a/app/models/book.rb
@@ -0,0 +1,28 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorite, dependent: :destroy
+  
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  
+  def favorited_by?(user)
+    favorite.exists?(user_id: user.id)
+  end
+  
+  def self.looks(search,word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?", "#{word}")
+    elsif search == "forword_match"
+      @book = Book.where("title LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?", "%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+
+
+end
---
+++ bookers2_789a/config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "homes#top"
+  get "home/about"=>"homes#about"
+  get 'search' => 'searches#search'
+  devise_for :users
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites,only: [:create,:destroy]
+    resources :book_comments, only: [:create,:destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships,only: [:create,:destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
