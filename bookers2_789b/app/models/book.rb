09c88c5159e274778fa8a4c8a8c45ace6767a249
yuna096740 <yyuusseeii1111@gmail.com>
2023-05-27 01:38:48 +0000

    task 8b done

--- bookers2_789b/app/models/book.rb
+++ bookers2_789b/app/models/book.rb
@@ -26,9 +26,12 @@ class Book < ApplicationRecord
 
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
-  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_this_week, -> { where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
-  
-  scope :created_each_date, -> (date){ where(created_at: (date).day.ago.all_day) }
-  
+
+  scope :created_each_date, -> (date){ where(created_at: date.days.ago.all_day) }
+  def self.book_week_count
+    (1..6).map { |date| created_each_date(date).count}.reverse
+  end
+
 end
