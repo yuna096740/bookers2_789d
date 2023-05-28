83550412fe8f823976f3c7ce37c196347a28b2fe
yuna096740 <yyuusseeii1111@gmail.com>
2023-05-28 01:02:45 +0000

    Initial Commit

--- bookers2_789d/app/javascript/packs/application.js
+++ bookers2_789d/app/javascript/packs/application.js
@@ -11,7 +11,13 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty = new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
 
 Rails.start()
 Turbolinks.start()
